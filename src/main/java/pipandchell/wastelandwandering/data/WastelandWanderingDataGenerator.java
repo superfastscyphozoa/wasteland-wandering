1ad902744b4c7f8e0302c1b18e4ae3bd0f9d20b8
superfastscyphozoa <mitchellelhage@gmail.com>
2024-09-09 04:29:23 +1000

    War never changes (initial commit)

---
+++ src/main/java/pipandchell/wastelandwandering/data/WastelandWanderingDataGenerator.java
@@ -0,0 +1,11 @@
+package pipandchell.wastelandwandering.data;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WastelandWanderingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
