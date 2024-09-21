674a241d7c945b1830d80491dd86bd4be1605e6f
superfastscyphozoa <mitchellelhage@gmail.com>
2024-09-21 19:45:38 +1000

    :3 slightly tweaked some stuff and made the itemgroups better

--- src/main/java/net/superfastscyphozoa/wastelandwandering/item/util/WawaItemGroups.java
+++ src/main/java/net/superfastscyphozoa/wastelandwandering/item/util/WawaItemGroups.java
@@ -14,16 +14,18 @@ import net.superfastscyphozoa.wastelandwandering.registry.RegisterBlocks;
 import net.superfastscyphozoa.wastelandwandering.registry.RegisterItems;
 
 public class WawaItemGroups {
-    public static final RegistryKey<ItemGroup> WAWA_BLOCKS_GROUP_KEY = register("wawa_blocks_group");
-    public static final RegistryKey<ItemGroup> WAWA_ITEMS_GROUP_KEY = register("wawa_items_group");
+    public static final RegistryKey<ItemGroup> WAWA_NATURAL_BLOCKS_GROUP = register("wawa_natural_blocks_group");
+    public static final RegistryKey<ItemGroup> WAWA_BUILDING_BLOCKS_GROUP = register("wawa_building_blocks_group");
+    public static final RegistryKey<ItemGroup> WAWA_COMBAT_GROUP = register("wawa_combat_group");
+    public static final RegistryKey<ItemGroup> WAWA_MISC_ITEMS_GROUP = register("wawa_misc_items_group");
 
     private static RegistryKey<ItemGroup> register(String id) {
         return RegistryKey.of(RegistryKeys.ITEM_GROUP, Identifier.of(WastelandWandering.MOD_ID, id));
     }
 
-    public static final ItemGroup WAWA_BLOCKS_GROUP = FabricItemGroup.builder()
-            .icon(() -> new ItemStack(RegisterBlocks.IRRADIATED_GRASS_BLOCK))
-            .displayName(Text.translatable("itemgroup.wasteland-wandering.wawa_blocks_group"))
+    public static final ItemGroup WAWA_NATURAL_BLOCKS = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(RegisterBlocks.IRRADIATED_TALL_GRASS))
+            .displayName(Text.translatable("itemgroup.wasteland-wandering.wawa_natural_blocks"))
             .entries((displayContext, entries) -> {
 
                 entries.add(RegisterBlocks.IRRADIATED_GRASS_BLOCK);
@@ -44,15 +46,47 @@ public class WawaItemGroups {
 
             }).build();
 
-    public static final ItemGroup WAWA_ITEMS_GROUP = FabricItemGroup.builder()
+    public static final ItemGroup WAWA_BUILDING_BLOCKS = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(RegisterBlocks.GRAY_ASPHALT))
+            .displayName(Text.translatable("itemgroup.wasteland-wandering.wawa_building_blocks"))
+            .entries((displayContext, entries) -> {
+
+                entries.add(RegisterBlocks.WHITE_ASPHALT);
+                entries.add(RegisterBlocks.LIGHT_GRAY_ASPHALT);
+                entries.add(RegisterBlocks.GRAY_ASPHALT);
+                entries.add(RegisterBlocks.BLACK_ASPHALT);
+                entries.add(RegisterBlocks.BROWN_ASPHALT);
+                entries.add(RegisterBlocks.RED_ASPHALT);
+                entries.add(RegisterBlocks.ORANGE_ASPHALT);
+                entries.add(RegisterBlocks.YELLOW_ASPHALT);
+                entries.add(RegisterBlocks.LIME_ASPHALT);
+                entries.add(RegisterBlocks.GREEN_ASPHALT);
+                entries.add(RegisterBlocks.CYAN_ASPHALT);
+                entries.add(RegisterBlocks.LIGHT_BLUE_ASPHALT);
+                entries.add(RegisterBlocks.BLUE_ASPHALT);
+                entries.add(RegisterBlocks.PURPLE_ASPHALT);
+                entries.add(RegisterBlocks.MAGENTA_ASPHALT);
+                entries.add(RegisterBlocks.PINK_ASPHALT);
+
+            }).build();
+
+    public static final ItemGroup WAWA_COMBAT = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(RegisterItems.DYNAMITE))
+            .displayName(Text.translatable("itemgroup.wasteland-wandering.wawa_combat"))
+            .entries((displayContext, entries) -> {
+
+                entries.add(RegisterItems.DYNAMITE);
+                entries.add(RegisterItems.MOLOTOV_COCKTAIL);
+                entries.add(RegisterItems.OIL);
+
+            }).build();
+
+    public static final ItemGroup WAWA_MISC_ITEMS = FabricItemGroup.builder()
             .icon(() -> new ItemStack(RegisterItems.OIL))
-            .displayName(Text.translatable("itemgroup.wasteland-wandering.wawa_items_group"))
+            .displayName(Text.translatable("itemgroup.wasteland-wandering.wawa_misc_items"))
             .entries((displayContext, entries) -> {
 
                 entries.add(RegisterItems.OIL);
-                entries.add(RegisterItems.DYNAMITE);
-                entries.add(RegisterItems.MOLOTOV_COCKTAIL);
-
                 entries.add(RegisterItems.MUTFRUIT);
                 entries.add(RegisterItems.STIMPAK);
 
@@ -61,8 +95,10 @@ public class WawaItemGroups {
     public static void registerWawaItemGroups() {
         WastelandWandering.LOGGER.info("Registering Item Groups for " + WastelandWandering.MOD_ID);
 
-        Registry.register(Registries.ITEM_GROUP, WAWA_BLOCKS_GROUP_KEY, WAWA_BLOCKS_GROUP);
-        Registry.register(Registries.ITEM_GROUP, WAWA_ITEMS_GROUP_KEY, WAWA_ITEMS_GROUP);
+        Registry.register(Registries.ITEM_GROUP, WAWA_NATURAL_BLOCKS_GROUP, WAWA_NATURAL_BLOCKS);
+        Registry.register(Registries.ITEM_GROUP, WAWA_BUILDING_BLOCKS_GROUP, WAWA_BUILDING_BLOCKS);
+        Registry.register(Registries.ITEM_GROUP, WAWA_COMBAT_GROUP, WAWA_COMBAT);
+        Registry.register(Registries.ITEM_GROUP, WAWA_MISC_ITEMS_GROUP, WAWA_MISC_ITEMS);
     }
 
 }
